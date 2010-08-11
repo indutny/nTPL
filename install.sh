8be4f185203ce75cc92d19ced8b6f96b9ab22e77
Fedor Indutny <fedor.indutny@gmail.com>
2010-08-11 20:57:17 +0700

    install.sh fixes

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-mkdir /tmp/nTPL && cd /tmp/nTPL && curl -# -L http://github.com/donnerjack13589/nTPL/tarball/master | tar xz --strip 1 && make && make install && echo "Installation complete"
+mkdir -p /tmp/nTPL && cd /tmp/nTPL && curl -# -L http://github.com/donnerjack13589/nTPL/tarball/master | tar xz --strip 1 && make && make install && echo "Installation complete"
