1517c100e1dd212b6e284c625f27356bac2c8ee0
Fedor Indutny <fedor.indutny@gmail.com>
2010-08-11 11:52:52 +0700

    Benchmarks and readme

---
+++ benchmarks/million_complex.js
@@ -0,0 +1,25 @@
+require("../lib/nTPL");
+
+var tpl = nTPL({
+	template: "./complex.html",
+	args: ["header", "list"]
+});
+
+var list = [{name: "red", current: true, url: "#Red"},
+      {name: "green", current: false, url: "#Green"},
+      {name: "blue", current: false, url: "#Blue"}];
+
+var output;
+
+var i = 1E6;
+var startTime = +new Date;
+
+while (i-->0){
+	output = tpl({
+		header: "Colors",
+		list: list
+	});
+}
+startTime = +new Date - startTime;
+
+console.log("Running time: " + startTime + "ms");
