d3c9f243e324211035a61ef7b41d44dec87f9acb
Fedor Indutny <fedor.indutny@gmail.com>
2010-08-13 19:22:26 +0700

    Fixed name changing

--- tests/tests/test-27.js
+++ tests/tests/test-27.js
@@ -1,7 +1,7 @@
 this.name = "Native mod: set name";
 this.run = function(test, nTPL, callback) {
 	
-	var a = nTPL("{%set name test; %}123")();
+	var a = nTPL("{%set name test %}123")();
 	var b = nTPL("test")();
 	
 	test.equal(a, b);	
--- tests/tests/test-30.js
+++
@@ -1,8 +0,0 @@
-this.name = "Native mod: set options / semicolon";
-this.run = function(test, nTPL, callback) {
-	
-	var a = nTPL("{%set args hello; %}{%= hello %}")({hello: "Hello world!"});
-	
-	test.equal(a, "Hello world!");	
-	
-}
