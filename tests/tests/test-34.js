09aacc9904f2335a0fe1446020a41c5df81c0121
Fedor Indutny <fedor.indutny@gmail.com>
2010-08-15 13:19:43 +0700

    Express.js tests

---
+++ tests/tests/test-34.js
@@ -0,0 +1,15 @@
+this.name = "Express.js integration";
+this.run = function(test, nTPL, callback) {
+	var fs = require("fs");
+	
+	fs.writeFileSync("./tests/file-34", "{%set args a1,a2 %}{%= a1 %}:{%= a2 %}");
+	
+	var a = nTPL.render("express.js@./tests/file-34")({
+		a1: 1,
+		a2: 2
+	});
+	nTPL.unwatch();
+	
+	test.equal(a, "1:2");
+	
+}
